6041c264200d4d11999b37a133b8bd3572366d7f
Longyuhai <465982013@qq.com>
2023-05-06 17:06:57 +0800

    这是一个测试用例

---
+++ test.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("呵呵！\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -1;
+//	int b = a << 1;
+//	printf("b = %d", b);
+//}
+
+
+//断点  F9切换断点
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", i);
+//	}
+//
+//	for (i = 0; i < 100; i++)
+//	{
+//		printf("%d ", 10 - i);
+//	}
+//}
+
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	int a = 20;
+//	int b = 10;
+//	int c = Add(a, b);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//
+//	return 0;
+//}
+
+//简单的递归函数，一直打印hehe
+//int main()
+//{
+//	printf("hehe\n");
+//	main();
+//	return 0;
+//}
